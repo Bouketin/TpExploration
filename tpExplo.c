f3a0f9fee32d43cb68be04f314a4f12abd1614c2
ThomasL-GD <59962750+ThomasL-GD@users.noreply.github.com>
2020-02-06 14:30:54 +0100

    Update tpExplo.c

--- tpExplo.c
+++ tpExplo.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 	//variables
-	int tamponDestination=0;
-	int destination=0;
-	
+	char destination[30];
+	char dest1[30] = "Blinguia\0";
+	char dest2[30] = "Afrat\0";
 	
 	//debut
 	printf("Salut les enfants, bienvenue dans le bus de l'ecole Blingue Premier ! Alors, vous voulez aller ou ?\n Pour aller a Blinguia, tapez 1, pour aller en Afrat, tapez 2\n");
 	
-	scanf("%d",&destination);
+	scanf("%s",&destination);
 	
-	while(destination != 1 && destination != 2){
+	while(strcmp(destination, dest1) !=0 && strcmp(destination, dest2) != 0){
 		printf("ah non ca c'est pas ma direction ca, choisissez autre chose\n");
 		
-		scanf("%d",&destination);
+		scanf("%s",&destination);
 	}
 	
-	if(destination==1){
+	if(strcmp(destination,dest1)==0){
 			printf("bienvenue a Blinguia, capitale historique de la magie fondee a la gloire de Blingue le Premier, tueur de l'Immortel et sauveur de l'humanite !\n");
 		}
 		
-	if(destination==2){
+	if(strcmp(destination,dest2)==0){
 		printf("bienvenue en Afrat, pays des Afraites, une contree pauvre et aride ou l'avenir se resume a un champ ou une mine. La grosse joie.\n");
 	}
+	
+	return 0;
 }
